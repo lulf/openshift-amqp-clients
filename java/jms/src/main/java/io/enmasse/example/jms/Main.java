e9cb5e3d4dd76d96f4644d53cefe07116ac2349e
Ulf Lilleengen <lulf@redhat.com>
2018-04-05 11:02:36 +0200

    Add JMS example

--- java/jms/src/main/java/io/enmasse/example/jms/Main.java
+++ java/jms/src/main/java/io/enmasse/example/jms/Main.java
@@ -1,31 +1,41 @@
 package io.enmasse.example.jms;
 
 import io.enmasse.example.common.AppCredentials;
-import io.vertx.core.Vertx;
 
-import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Optional;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
-import javax.jms.JMSConnectionFactory;
+import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 public class Main {
 
     public static void main(String [] args) throws Exception {
         String address = Optional.ofNullable(System.getenv("ADDRESS")).orElse("myqueue");
         AppCredentials appCredentials = AppCredentials.create();
- 
-        
-        ConnectionFactory  connectionFactory = new
+
+        Context context = fromCredentials(appCredentials, address);
+
+        ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("factoryLookup");
+        Destination destination = (Destination) context.lookup("destinationLookup");
+
+        JMSConsumer consumer = new JMSConsumer(connectionFactory, destination);
+        JMSProducer producer = new JMSProducer(connectionFactory, destination);
+
+        Executor executor = Executors.newFixedThreadPool(2);
+        executor.execute(consumer);
+        executor.execute(producer);
     }
 
-    private static Context fromCredentials(AppCredentials credentials, String queueName) {
+    private static Context fromCredentials(AppCredentials credentials, String queueName) throws NamingException {
         
         Hashtable<Object, Object> env = new Hashtable<>();
-        env.put("connectionfactory.myFactory", String.format("amqps://%s:%s@%s:%d", credentials.getUsername(), credentials.getPassword(), 
-        Context context = new InitialContext(env);
-        return context;
+        env.put("connectionfactory.factoryLookup", String.format("amqps://%s:%d?jms.username=%s&jms.password=%s", credentials.getHostname(), credentials.getPort(), credentials.getUsername(), credentials.getPassword()));
+        env.put("queue.queueLookup", queueName);
+        return new InitialContext(env);
     }
 }
